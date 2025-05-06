b3c108b447c5b9b1389fdc647a7e7833a4b91f4f
Ryo-Fujimori <fujimori1072@gmail.com>
2025-05-06 17:31:45 +0900

    Add Post Controller Views

---
+++ app/controllers/User/posts_controller.rb
@@ -0,0 +1,39 @@
+class User::PostsController < ApplicationController
+  before_action :set_post, only: [ :edit, :update, :destroy ]
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "Post was successfully created."
+    else
+      render :new, status: :unprocessable_entity, notice: "Post was not created."
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "Post was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity, notice: "Post was not updated."
+    end
+  end
+
+  def destroy
+    @post.destroy!
+    redirect_to posts_url, notice: "Post was successfully destroyed."
+  end
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
+
   root "posts#index"
-  resources :posts
+  resources :posts, only: %i[ index show ]
+  resources :users, only: [ :index, :show, :edit, :update ]
+
+  #ログイン中のみ新規ポスト作成、編集、削除が可能
+  namespace :users do
+    resources :posts, only: %i[ new create edit update destroy ]
+  end
 end
