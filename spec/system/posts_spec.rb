6d2263ff071dfc4d8d1b40511536107431ff235f
Ryo-Fujimori <fujimori1072@gmail.com>
2025-05-10 18:28:41 +0900

    revie fix Test 日本語化 使わないファイル、ディレクトリ削除

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -7,53 +7,46 @@ RSpec.describe "Posts", type: :system do
   before do
     driven_by(:rack_test)
   end
-  # ポストが作成されることを確認する
-  it "creates a post" do
+  it "ポストが作成されること" do
     visit new_post_path
     fill_in "コンテンツ", with: "This is a test post."
     click_button "保存"
-    expect(page).to have_content("Post was successfully created.")
+    expect(page).to have_content("作成しました")
     expect(page).to have_content("This is a test post.")
   end
-  # post詳細ページに遷移することを確認する
-  it "shows a post" do
+  it "post詳細ページに遷移すること" do
     visit post_path(post)
     expect(page).to have_content("This is a test post.")
   end
-  # postが更新されることを確認する
-  it "updates a post" do
+  it "postが更新されること" do
     visit edit_post_path(post)
     fill_in "コンテンツ", with: "This is an updated test post."
     click_button "保存"
-    expect(page).to have_content("Post was successfully updated.")
+    expect(page).to have_content("更新しました。")
     expect(page).to have_content("This is an updated test post.")
   end
-  # postが削除されることを確認する
-  it "destroys a post" do
+  it "postが削除されること" do
     visit post_path(post)
     click_link "削除"
-    expect(page).to have_content("Post was successfully destroyed.")
+    expect(page).to have_content("投稿を削除しました。")
     expect(page).not_to have_content("This is a test post.")
   end
-  # contentが空の投稿を作成しようとしたときにエラーメッセージが表示されることを確認する
-  it "does not create a post with empty content" do
+  it "空の投稿を作成しようとしたときにエラーメッセージが表示されること" do
     visit new_post_path
     fill_in "コンテンツ", with: ""
-    click_button "保存"
-    expect(page).to have_content("Contentを入力してください")
+    click_button "投稿する"
+    expect(page).to have_content("本文を入力してください")
   end
-  # contentが空の投稿を更新しようとしたときにエラーメッセージが表示されることを確認する
-  it "does not update a post with empty content" do
+  it "空の投稿を更新しようとしたときにエラーメッセージが表示されること" do
     visit edit_post_path(post)
     fill_in "コンテンツ", with: ""
-    click_button "保存"
-    expect(page).to have_content("Contentを入力してください")
+    click_button "投稿する"
+    expect(page).to have_content("本文を入力してください")
   end
-  # contentが200文字以上の投稿を作成しようとしたときにエラーメッセージが表示されることを確認する
-  it "does not create a post with content over 200 characters" do
+  it "200文字以上の投稿を作成しようとしたときにエラーメッセージが表示されること" do
     visit new_post_path
     fill_in "コンテンツ", with: "a" * 201
-    click_button "保存"
+    click_button "投稿する"
     expect(page).to have_content("Contentは200文字以内で入力してください")
   end
 end
