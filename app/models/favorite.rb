9c154bac064d4202d809c9826bd41c76955f59e0
Ryo-Fujimori <fujimori1072@gmail.com>
2025-05-11 15:39:03 +0900

    add favorite model

---
+++ app/models/favorite.rb
@@ -0,0 +1,9 @@
+class Favorite < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  validates :user_id, uniqueness: { scope: :post_id }
+  validates :user_id, presence: true
+  validates :post_id, presence: true
+  
+end
