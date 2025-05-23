b60010f2ee401dc366d7920c7d255ef2d4932864
Ryo-Fujimori <fujimori1072@gmail.com>
2025-05-23 22:44:55 +0900

    テスト追加　user_path に遷移(未完成)

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 Rails.root.glob('spec/support/*.rb').each { |f| require f }
 
 RSpec.describe 'Users', type: :system do
+  let(:user) { User.create(email: 'test@example.com', password: 'password') }
+
   before do
     driven_by(:rack_test)
   end
 
-  # User一覧画面に遷移することを確認する
-  it 'shows the user list' do
+  it 'User一覧画面に遷移することを確認する' do
     visit users_path
     expect(page).to have_content('ユーザー一覧')
   end
 
-  # User新規登録ができることを確認する
-  it 'creates a user' do
+  it 'User新規登録ができることを確認する' do
     visit new_user_registration_path
     fill_in 'user_email', with: 'test@user.com'
     fill_in 'user_password', with: 'password'
@@ -22,8 +22,7 @@ RSpec.describe 'Users', type: :system do
     expect(page).to have_content('アカウント登録が完了しました。')
   end
 
-  # User新規登録ができないことを確認する
-  it 'does not create a user with empty email' do
+  it 'User新規登録ができないことを確認する' do
     visit new_user_registration_path
     fill_in 'user_email', with: ''
     fill_in 'user_password', with: 'password'
@@ -32,8 +31,16 @@ RSpec.describe 'Users', type: :system do
     expect(page).to have_content('Eメールを入力してください')
   end
 
-  # User詳細画面に遷移することを確認する
-  it 'shows the user detail' do
+  it 'ユーザー一覧から、ユーザーをクリックして user_path に遷移していることを確認する' do
+    sign_in user, scope: :user
+    click_link 'ユーザー一覧'
+    expect(page).to have_content('ユーザー一覧')
+    click_on 'admin@init.com'
+    have_current_path(user_path(1))
+    expect(page).to have_content('ユーザー詳細')
+  end
+
+  it 'User詳細画面に遷移することを確認する' do
     user = User.create(email: 'user@test.com ', password: 'password')
     visit user_path(user.id)
     expect(page).to have_content('ユーザー詳細')
